8a404be5b709584599402ce5941c10e8398abfe0
Ramadhan-apergu <ramadhan.apergu@gmail.com>
2024-11-05 11:25:24 +0700

    setting vercel.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config()
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'privy_monitoring_db',
-  password: 'Ramadhan@12',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
