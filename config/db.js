aa8ac2edff42aec363b9d683d17d3e1f7143e9a6
Ramadhan-apergu <ramadhan.apergu@gmail.com>
2024-11-05 11:40:16 +0700

    set env

--- config/db.js
+++ config/db.js
@@ -1,12 +1,21 @@
 const { Pool } = require('pg');
-require('dotenv').config()
+require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
+
+// const { Pool } = require('pg');
+// require('dotenv').config()
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASS,
+//   port: process.env.DB_PORT,
+// });
+
+// module.exports = pool;
